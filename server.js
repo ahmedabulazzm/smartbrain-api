562220d925005df50f85da1f2aa466578caa8f46
ahmedabulazzm <ahmed.abulazm1@gmail.com>
2018-09-03 16:28:40 +0200

    changing db

--- server.js
+++ server.js
@@ -12,9 +12,9 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-fluffly-89284',
     user : 'postgres',
-    password : 'Ahmedshiko1',
+    password : '',
     database : 'smartbrain'
   }
 });
